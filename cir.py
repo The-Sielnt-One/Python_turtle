54232d3df625415b4c8742bd8e9a3be2d48ac87d
The Silent One <safou.mor@live.fr>
2018-01-31 09:48:02 +0100

    Add files via upload

---
+++ cir.py
@@ -0,0 +1,26 @@
+from pygame import *
+from turtle import *
+from time import*
+#for i in range(4):
+    #lt(90)
+    #sleep(2)
+    #circle(-10,180)
+
+#for i in range(4):
+    #lt(90)
+    #sleep(2)
+    #circle(10,180)
+
+#x=10
+#for i in range(4):
+    #for j in range(1,100):
+        #fd(pi*x/2/j)
+        #rt(1)
+    #up()
+    #home()
+    #down()
+    #lt(90*(i+1))
+
+
+sleep(2)
+bye()
